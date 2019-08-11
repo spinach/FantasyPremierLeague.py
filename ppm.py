75e79f24a792675665be877c782c0c63fa04b4b1
Thomas Kunwar <yathomasi@gmail.com>
2019-08-11 11:24:15 +0545

    Removed ZeroDivisionError

--- ppm.py
+++ ppm.py
@@ -3,19 +3,19 @@ import json
 import csv
 import argparse
 
-FPL_URL = "https://fantasy.premierleague.com/drf/"
+FPL_URL = "https://fantasy.premierleague.com/api/"
 USER_SUMMARY_SUBURL = "element-summary/"
-LEAGUE_CLASSIC_STANDING_SUBURL = "leagues-classic-standings/"
-LEAGUE_H2H_STANDING_SUBURL = "leagues-h2h-standings/"
+LEAGUE_CLASSIC_STANDING_SUBURL = "leagues-classic/"
+LEAGUE_H2H_STANDING_SUBURL = "leagues-h2h/"
 TEAM_ENTRY_SUBURL = "entry/"
 PLAYERS_INFO_SUBURL = "bootstrap-static"
-PLAYERS_INFO_FILENAME = "allPlayersInfo.json"
+PLAYERS_INFO_FILENAME = "output/allPlayersInfo.json"
 
 USER_SUMMARY_URL = FPL_URL + USER_SUMMARY_SUBURL
 PLAYERS_INFO_URL = FPL_URL + PLAYERS_INFO_SUBURL
 START_PAGE = 1
 
-# Download all player data: https://fantasy.premierleague.com/drf/bootstrap-static
+# Download all player data: https://fantasy.premierleague.com/api/bootstrap-static
 def getPlayersInfo():
     r = requests.get(PLAYERS_INFO_URL)
     jsonResponse = r.json()
@@ -37,6 +37,9 @@ def writeToFile(countOfplayersPicked, fileName):
         for row in countOfplayersPicked:
             csv_out.writerow(row)
 
+def weird_division(n, d):
+    return n / d if d else 0
+
 # Main Script
 
 
@@ -45,8 +48,8 @@ playerElementIdToNameMap = {}
 allPlayers = getAllPlayersDetailedJson()
 playerElementIdToNameMap[0] = ",".join(["Name", "Position", "Total Score", "Minutes Played", "Cost", "points_per_game", "points_per_game_per_million", "Bonus per 90", "Points per 90", "Points per million", "Points per million per 90"])
 for element in allPlayers["elements"]:
-    if element["minutes"] < 1000:
-        continue
+#     if element["minutes"] < 1000:
+#         continue
     totalScore = element["total_points"]
     minutesPlayed = element["minutes"]
     cost = element["now_cost"] / 10.0
@@ -54,14 +57,14 @@ for element in allPlayers["elements"]:
     points_per_game = float(element["points_per_game"])
     points_per_game_per_million = round(points_per_game / cost, 2)
     appearances = minutesPlayed / 90.0
-    pointsPer90 = round(totalScore / appearances, 2)
-    pointsPerMillion = round(totalScore / cost, 2)
-    pointsPerMillionPer90 = round(pointsPer90 / cost, 2)
-    bonusPer90 = round(element["bonus"] / appearances, 2)
+    pointsPer90 = round(weird_division(totalScore , appearances), 2)
+    pointsPerMillion = round(weird_division(totalScore , cost), 2)
+    pointsPerMillionPer90 = round(weird_division(pointsPer90 , cost), 2)
+    bonusPer90 = round(weird_division(element["bonus"] , appearances), 2)
     name = element["web_name"].encode('ascii', 'ignore')
     playerElementIdToNameMap[element["id"]] = ",".join([name, str(position), str(totalScore), str(minutesPlayed), str(cost), str(points_per_game), str(points_per_game_per_million), str(bonusPer90), str(pointsPer90), str(pointsPerMillion), str(pointsPerMillionPer90)])
 
 print(playerElementIdToNameMap)
-with open("ppm.csv", 'w') as f:
+with open("output/ppm.csv", 'w') as f:
     for key, value in playerElementIdToNameMap.items():
         f.write(value + "\n")
